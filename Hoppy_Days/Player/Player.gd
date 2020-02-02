f69e02a3d517a8176b30a21784ee0a97860d3486
prodes13 <ciobotaru.florin@gmail.com>
2020-02-02 11:03:18 +0200

    hoopy started

---
+++ Hoppy_Days/Player/Player.gd
@@ -0,0 +1,39 @@
+extends KinematicBody2D
+
+var motion = Vector2(0, 0)
+const SPEED = 1500
+const GRAVITY = 300
+# constant for the floor 
+const UP = Vector2(0, -1)
+const JUMP_SPEED = 5000
+
+signal animate
+
+func _physics_process(delta):
+	apply_gravity()
+	jump()
+	move()
+	animate()
+	move_and_slide(motion, UP)
+#	print(delta)
+
+func apply_gravity():
+	if is_on_floor():
+		motion.y = 0
+	else: 
+		motion.y += GRAVITY
+
+func jump():
+	if Input.is_action_pressed("jump") and is_on_floor():
+		motion.y -= JUMP_SPEED
+
+func move():
+	if Input.is_action_pressed("left") and not Input.is_action_pressed("right"):
+		motion.x = -SPEED
+	elif Input.is_action_pressed("right") and not Input.is_action_pressed("left"):
+		motion.x = SPEED
+	else:
+		motion.x = 0
+		
+func animate():
+	emit_signal("animate", motion)
--- Lonny_Lips/LonnyLips.gd
+++ Lonny_Lips/LonnyLips.gd
@@ -1,27 +1,54 @@
 extends Control 
 
 var player_words = []
-var story = "Once upon a time %s watched %s and thought it was the %s movie off all time!"
-var prompts = ["Individus", "Minions", "greatest"]
+
+var current_story = {
+		prompts = ["a name", "a noun", "adverb", "adjective"],
+		story = "Once upon a time %s watched %s and thought it was the %s movie off all time! It was a %s day!"		
+		}
 
 onready var PlayerText = $VBoxContainer/HBoxContainer/PlayerText
 onready var DisplayText = $VBoxContainer/DisplayText
+onready var StatusText = $VBoxContainer/HBoxContainer/Label
 
-#func _ready():
-#	print(story % prompts)
-#	DisplayText.text = story % prompts
-
-
-func update_DisplayText(words):
-	DisplayText.text = words
+func _ready():
+	check_player_words_length()
 	PlayerText.clear()
+	PlayerText.grab_focus()
 
-func _on_PlayerText_text_entered(new_text):
-	update_DisplayText(new_text)
+
+func _on_PlayerText_text_entered():
+	add_to_player_words()
 
 func _on_TextureButton_pressed():
-	var words = PlayerText.text
-	update_DisplayText(words)
+	if is_story_done():
+		get_tree().reload_current_scene()
+	else:
+		add_to_player_words()
+		PlayerText.grab_focus()
 
 func add_to_player_words():
-	pass
+	player_words.append(PlayerText.text)
+	PlayerText.clear()
+	check_player_words_length()
+
+func is_story_done():
+	return player_words.size() == current_story.prompts.size()
+	
+func check_player_words_length():
+	if is_story_done():
+		end_game()
+	else:
+		prompt_player()
+
+func tell_story():
+	DisplayText.text = current_story.story % player_words
+
+func prompt_player():
+	DisplayText.text = "May I have " + current_story.prompts[player_words.size()] + " please?"
+	
+func end_game():
+	PlayerText.queue_free()
+	tell_story()
+	StatusText.text = "Again"
+#	PlayerText.visible = false
