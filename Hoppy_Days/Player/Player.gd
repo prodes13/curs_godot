d2e9851a7ad978e9337fc4bdf134435d42843dc0
prodes13 <ciobotaru.florin@gmail.com>
2020-02-03 00:13:29 +0200

    almost finished the game

--- Hoppy_Days/Player/Player.gd
+++ Hoppy_Days/Player/Player.gd
@@ -2,10 +2,13 @@ extends KinematicBody2D
 
 var motion = Vector2(0, 0)
 const SPEED = 1500
-const GRAVITY = 300
+const GRAVITY = 150
 # constant for the floor 
 const UP = Vector2(0, -1)
-const JUMP_SPEED = 5000
+const JUMP_SPEED = 3500
+const WORLD_LIMIT = 4000
+
+var lives = 3
 
 signal animate
 
@@ -18,14 +21,20 @@ func _physics_process(delta):
 #	print(delta)
 
 func apply_gravity():
+	if position.y > WORLD_LIMIT:
+		end_game()
 	if is_on_floor():
 		motion.y = 0
+	elif is_on_ceiling():
+		motion.y = 1
 	else: 
 		motion.y += GRAVITY
 
 func jump():
 	if Input.is_action_pressed("jump") and is_on_floor():
 		motion.y -= JUMP_SPEED
+		$JumpSFX.stream = load("res://SFX/jump1.ogg")
+		$JumpSFX.play()
 
 func move():
 	if Input.is_action_pressed("left") and not Input.is_action_pressed("right"):
@@ -37,3 +46,16 @@ func move():
 		
 func animate():
 	emit_signal("animate", motion)
+
+func end_game():
+	get_tree().change_scene("res://Levels/GameOver.tscn")
+
+func hurt():
+	position.y -= 1
+	yield(get_tree(), "idle_frame")
+	motion.y -= JUMP_SPEED
+	lives -= 1
+	$PainSFX.stream = load("res://SFX/pain.ogg")
+	$PainSFX.play()
+	if lives < 0:
+		end_game()
