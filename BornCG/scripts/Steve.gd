8a4f3d2863f834b432b1f1f40a5f0ccae4e6e97e
prodes13 <ciobotaru.florin@gmail.com>
2020-02-24 08:12:20 +0200

    solving the mesh bug - big file

--- BornCG/scripts/Steve.gd
+++ BornCG/scripts/Steve.gd
@@ -1,7 +1,8 @@
 extends KinematicBody
 
 var velocity = Vector3(0, 0, 0)
-const SPEED = 6
+const SPEED = 12
+const ANGULAR_SPEED = deg2rad(9)
 
 func _ready():
 	pass
@@ -11,8 +12,10 @@ func _physics_process(delta):
 		velocity.x = 0
 	elif Input.is_action_pressed("right"):
 		velocity.x = SPEED
+		$MeshInstance.rotate_z(-ANGULAR_SPEED)
 	elif Input.is_action_pressed("left"):
 		velocity.x = -SPEED
+		$MeshInstance.rotate_z(ANGULAR_SPEED)
 	else:
 		velocity.x = lerp(velocity.x, 0, 0.1)
 	#math approach form to float
@@ -20,12 +23,20 @@ func _physics_process(delta):
 		velocity.z = 0
 	elif Input.is_action_pressed("up"):
 		velocity.z = -SPEED
+		$MeshInstance.rotate_x(-ANGULAR_SPEED)
 	elif Input.is_action_pressed("down"):
 		velocity.z = SPEED
+		$MeshInstance.rotate_x(ANGULAR_SPEED)
 	else:
 		velocity.z = lerp(velocity.z, 0, 0.1)
 		
 	move_and_slide(velocity)
-	
-#continue from 
-#https://www.youtube.com/watch?v=VWy4ErYd630&t=908s
+
+#		motion.y = clamp(motion.y - SPEED, -MAX_SPEED, 0)	
+
+func _on_Enemy_body_entered(body):
+	if body.name == "Steve":
+		get_tree().change_scene("res://GameOver.tscn")
+
+# last video 
+#https://www.youtube.com/watch?v=rS-PcTTiGtA
