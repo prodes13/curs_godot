ea2761b230de9011f10c4fbf8ba364829a03844a
prodes13 <ciobotaru.florin@gmail.com>
2020-02-25 12:53:53 +0200

    FPS game in GODOT started

---
+++ FPSgame/Player/Player.gd
@@ -0,0 +1,55 @@
+extends KinematicBody
+
+export var speed = 10
+export var acceleration = 5
+export var gravity = 0.98
+export var jump_power = 30
+export var mouse_sensivity = 0.3
+
+onready var head = $Head
+onready var camera = $Head/Camera
+
+var velocity = Vector3()
+var camera_x_rotation = 0
+
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func _input(event):
+	if event is InputEventMouseMotion:
+		head.rotate_y(deg2rad(-event.relative.x * mouse_sensivity))
+		
+		var x_delta = event.relative.y * mouse_sensivity
+		if camera_x_rotation + x_delta > -90 and camera_x_rotation + x_delta < 90:
+			camera.rotate_x(deg2rad(-x_delta))
+			camera_x_rotation += x_delta
+
+func _process(delta):
+	if Input.is_action_just_pressed("ui_cancel"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+
+func _physics_process(delta):
+	var head_basis = head.get_global_transform().basis
+	
+	var direction = Vector3()
+	if Input.is_action_pressed("move_forward"):
+		direction -= head_basis.z
+	elif Input.is_action_pressed("move_backward"):
+		direction += head_basis.z
+	if Input.is_action_pressed("move_left"):
+		direction -= head_basis.x
+	elif Input.is_action_pressed("move_right"):
+		direction += head_basis.x
+	direction = direction.normalized()
+	
+#	create fps walking
+	velocity = velocity.linear_interpolate(direction * speed, acceleration * delta)
+	velocity.y -= gravity
+	
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y += jump_power
+		
+	velocity = move_and_slide(velocity, Vector3.UP)
+	
+#	https://www.youtube.com/watch?v=UV-bhtb3734&list=PLs1xoSUcO4GbKg2w3GcTPWqSt8Cq_j1p4&index=3&t=9s
+	
