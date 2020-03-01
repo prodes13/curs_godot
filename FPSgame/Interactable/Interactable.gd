a5afb28baf6c8503aef9f10bea1061f1a64a585c
prodes13 <ciobotaru.florin@gmail.com>
2020-03-01 22:48:12 +0200

    interaction raycast

--- FPSgame/Interactable/Interactable.gd
+++ FPSgame/Interactable/Interactable.gd
@@ -6,7 +6,4 @@ func get_interaction_text():
 	return "Interact"
 
 func interact():
-	print("Interacted with %s" % name)	
-
-#   https://www.youtube.com/watch?v=5YT4bFw9k4U
-
+	print("Interacted with %s" % name)
