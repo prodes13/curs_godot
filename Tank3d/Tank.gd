5e95401f8335639e9ccd4dfe05d1abc4c500b399
prodes13 <ciobotaru.florin@gmail.com>
2020-03-15 22:30:02 +0200

    added bullet into the game

--- Tank3d/Tank.gd
+++ Tank3d/Tank.gd
@@ -9,8 +9,15 @@ export var mouse_sensivity = 0.3
 var STEER_ANGLE = 0.02
 
 onready var head = $Turret
-onready var camera = $Camera
+onready var camera = $Turret/Camera
 onready var body = $Body
+onready var bullet_point = $Turret/Direction
+
+export var bullet_speed = 900
+export var fire_rate = 1
+
+var bullet = preload("res://bullet/Bullet.tscn")
+var can_fire = true
 
 var velocity = Vector3()
 var camera_x_rotation = 0
@@ -27,9 +34,20 @@ func _input(event):
 #			camera.rotate_x(deg2rad(-x_delta))
 #			camera_x_rotation += x_delta
 
-func _process(_delta):
+func _process(delta):
 	if Input.is_action_just_pressed("ui_cancel"):
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	if Input.is_action_pressed("fire") and can_fire:
+		var bullet_instance = bullet.instance()
+		bullet_instance.transform.origin = bullet_point.global_transform.origin
+		bullet_instance.rotation_degrees = head.rotation_degrees
+#		bullet_instance.apply_impulse(Vector3(), Vector3(0, 0, bullet_speed).rotated(rotation_degrees, 0))
+		var dest_vector = bullet_point.get_global_transform().origin - get_global_transform().origin
+		bullet_instance.apply_impulse(Vector3(), dest_vector*delta*bullet_speed)
+		get_tree().get_root().add_child(bullet_instance)
+		can_fire = false
+		yield(get_tree().create_timer(fire_rate), "timeout")
+		can_fire = true
 
 func _physics_process(delta):
 	var body_basis = body.get_global_transform().basis
