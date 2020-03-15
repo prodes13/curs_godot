3badf6eca674823539afc59e612fa35ab74d00be
prodes13 <ciobotaru.florin@gmail.com>
2020-03-15 23:39:26 +0200

    added cars to shoot at

--- Tank3d/Tank.gd
+++ Tank3d/Tank.gd
@@ -8,10 +8,11 @@ export var mouse_sensivity = 0.3
 
 var STEER_ANGLE = 0.02
 
-onready var head = $Turret
+onready var turret = $Turret
+onready var gun = $Turret/Gun
 onready var camera = $Turret/Camera
 onready var body = $Body
-onready var bullet_point = $Turret/Direction
+onready var bullet_point = $Turret/Gun/Direction
 
 export var bullet_speed = 900
 export var fire_rate = 1
@@ -20,19 +21,19 @@ var bullet = preload("res://bullet/Bullet.tscn")
 var can_fire = true
 
 var velocity = Vector3()
-var camera_x_rotation = 0
+var gun_x_rotation = 0
 
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 func _input(event):
 	if event is InputEventMouseMotion:
-		head.rotate_y(deg2rad(-event.relative.x * mouse_sensivity))
+		turret.rotate_y(deg2rad(-event.relative.x * mouse_sensivity))
 		
 		var x_delta = event.relative.y * mouse_sensivity
-#		if camera_x_rotation + x_delta > -90 and camera_x_rotation + x_delta < 90:
-#			camera.rotate_x(deg2rad(-x_delta))
-#			camera_x_rotation += x_delta
+		if gun_x_rotation + x_delta > -6 and gun_x_rotation + x_delta < 8:
+			gun.rotate_x(deg2rad(-x_delta))
+			gun_x_rotation += x_delta
 
 func _process(delta):
 	if Input.is_action_just_pressed("ui_cancel"):
@@ -40,7 +41,7 @@ func _process(delta):
 	if Input.is_action_pressed("fire") and can_fire:
 		var bullet_instance = bullet.instance()
 		bullet_instance.transform.origin = bullet_point.global_transform.origin
-		bullet_instance.rotation_degrees = head.rotation_degrees
+		bullet_instance.rotation_degrees = turret.rotation_degrees
 #		bullet_instance.apply_impulse(Vector3(), Vector3(0, 0, bullet_speed).rotated(rotation_degrees, 0))
 		var dest_vector = bullet_point.get_global_transform().origin - get_global_transform().origin
 		bullet_instance.apply_impulse(Vector3(), dest_vector*delta*bullet_speed)
